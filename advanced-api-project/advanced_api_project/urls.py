4ec1bcfbaf71e4de9311f7c82194005bef4892f3
limo-v <limookibet02@gmail.com>
2024-12-02 05:02:14 +1100

    main

--- advanced-api-project/advanced_api_project/urls.py
+++ advanced-api-project/advanced_api_project/urls.py
@@ -1,4 +1,4 @@
-"""advanced-api-project URL Configuration
+"""advanced_api_project URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.0/topics/http/urls/
