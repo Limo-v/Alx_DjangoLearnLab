698d229a689c33b430eed5fe9f71d56f718c2104
limo-v <limookibet02@gmail.com>
2024-12-15 11:36:08 +0300

    Task 0

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -1,7 +1,8 @@
 from rest_framework import serializers
 from .models import CustomUser
 from rest_framework.authtoken.models import Token
-
+"serializers.CharField()"
+"get_user_model().objects.create_user"
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = CustomUser
