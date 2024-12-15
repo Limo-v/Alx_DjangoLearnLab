1f4d86d999ea6ea101b848784620117643ee0558
limo-v <limookibet02@gmail.com>
2024-12-15 12:02:30 +0300

    Task 4

--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -3,7 +3,7 @@ from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from .models import Post, Comment
 from .serializers import PostSerializer, CommentSerializer
 "Post.objects.filter(author__in=following_users).order_by"
-
+"generics.get_object_or_404(Post, pk=pk)"
 class PostViewSet(viewsets.ModelViewSet):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
